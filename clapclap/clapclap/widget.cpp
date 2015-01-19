f9a9200df06b6ba4137f6e74c0f9c50128ab5c25
Felix Hofmann <felix.hofmann@rustyy.de>
2015-01-20 00:43:45 +0100

    seems to work

--- clapclap/clapclap/widget.cpp
+++ clapclap/clapclap/widget.cpp
@@ -1,17 +1,9 @@
 #include "widget.h"
 #include "ui_widget.h"
 #include <QMediaPlayer>
-
-//#include <QIODevice>
-//#include <QAudio>
-//#include <QAudioInput>
-//#include <QAudioDeviceInfo>
-//#include <QAudioFormat>
 #include <QDebug>
-//#include <QByteArray>
 
 const int BufferSize = 14096;
-
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
@@ -36,28 +28,35 @@ Widget::~Widget()
 
 void Widget::initAudio()
 {
+
     m_format.setSampleRate(8000);
+    // Channels set to mono.
+    m_format.setChannelCount(1);
+    // Set sample size.
+    m_format.setSampleSize(8);
+    //    m_format.setSampleType(QAudioFormat::UnSignedInt ); //Sample type as usigned integer sample
+    m_format.setSampleType(QAudioFormat::SignedInt);
+    // Byte order.
+    m_format.setByteOrder(QAudioFormat::LittleEndian);
+    // Codec as simple audio/pcm.
+    m_format.setCodec("audio/pcm");
 
-    m_format.setChannelCount(1); //set channels to mono
-    m_format.setSampleSize(16); //set sample sze to 16 bit
-    m_format.setSampleType(QAudioFormat::UnSignedInt ); //Sample type as usigned integer sample
-    m_format.setByteOrder(QAudioFormat::LittleEndian); //Byte order
-    m_format.setCodec("audio/pcm"); //set codec as simple audio/pcm
-
-
+    // Check if configured audioformat is supported.
     QAudioDeviceInfo infoIn(QAudioDeviceInfo::defaultInputDevice());
     if (!infoIn.isFormatSupported(m_format)) {
-        //Default format not supported - trying to use nearest
+        qWarning() << "Default format not supported - trying to use nearest.";
         m_format = infoIn.nearestFormat(m_format);
     }
 
+    // Create audioinput.
     createAudio();
 }
 
 void Widget::createAudio()
 {
-    if(m_input != 0) {
-        disconnect(m_input,0,this,0);
+    if(m_input != 0)
+    {
+        disconnect(m_input, 0, this, 0);
         m_input = 0;
     }
 
@@ -66,45 +65,70 @@ void Widget::createAudio()
 
 void Widget::readMore()
 {
-
+    // Exit if no audioinput is present.
     if(!m_audioinput)
         return;
 
-    //Check the number of samples in input buffer
+    // Check data available to read.
     qint64 len = m_audioinput->bytesReady();
 
     if(len > 4096) {
         len = 4096;
     }
 
+    // Read input stream and store into our buffer.
     qint64 l = m_input->read(m_buffer.data(), len);
-    QByteArray test = m_input->readAll();
+
+    /*************************************************************************************************
+     * @TODO: Clean up and documentation the following lines.
+     */
 
 
+
+    const int channelBytes = m_format.sampleSize() / 8;
+    const int sampleBytes = m_format.channelCount() * channelBytes;
+    const int numSamples = len / sampleBytes;
+    quint32 maxValue = 0;
+    const unsigned char *ptr = reinterpret_cast<const unsigned char *>(m_buffer.data());
 
     if(l > 0) {
-        //Assign sound samples to short array
-        short* resultingData = (short*)m_buffer.data();
-        short *outdata=resultingData;
 
-        qWarning() << resultingData;
+        for (int i = 0; i < numSamples; ++i) {
+            for (int j = 0; j < m_format.channelCount(); ++j) {
+                quint32 value = 0;
+
+                value = qAbs(*reinterpret_cast<const qint8*>(ptr));
+
+
+                maxValue = qMax(value, maxValue);
+                ptr += channelBytes;
+            }
+        }
+        qWarning() << maxValue;
+        maxValue = qMin(maxValue, quint32(127));
+        qreal level = qreal(maxValue) / 127;
+        qWarning() << level;
+
     }
 }
 
 void Widget::on_startButton_clicked()
 {
-//    player->setMedia(QUrl::fromLocalFile("../clapclap/music.mp3"));
-//    player->setVolume(50);
-//    player->play();
+    //    player->setMedia(QUrl::fromLocalFile("../clapclap/music.mp3"));
+    //    player->setVolume(50);
+    //    player->play();
 
     // Start microphone listening.
     m_input = m_audioinput->start();
-    //connect readyRead signal to readMre slot.
-    //Call readmore when audio samples fill in inputbuffer
+
+    // Connect readyRead signal to readMre slot.
     connect(m_input, SIGNAL(readyRead()), SLOT(readMore()));
 }
 
 void Widget::on_stopButton_clicked()
 {
-    player->stop();
+    //    player->stop();
+
+    // Stop audioinput.
+    m_audioinput->stop();
 }
